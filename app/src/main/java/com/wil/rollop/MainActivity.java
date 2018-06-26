f880384465399fa32f3fd13407de1a2cd68153a8
william <william@rubert.com.br>
2018-06-26 16:42:47 -0300

    Add readme.md, show clicks on dices buttons and clear rollDisplay

--- app/src/main/java/com/wil/rollop/MainActivity.java
+++ app/src/main/java/com/wil/rollop/MainActivity.java
@@ -6,14 +6,44 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView rollDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rollDisplay = (TextView) findViewById(R.id.rollDisplay);
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.d100){
+            Button d100 = (Button) v;
+            rollDisplay.append(String.format("%s", d100.getText()));
+        }else if(v.getId()==R.id.d20){
+            Button d20 = (Button) v;
+            rollDisplay.append(String.format("%s", d20.getText()));
+        }else if(v.getId()==R.id.d12){
+            Button d12 = (Button) v;
+            rollDisplay.append(String.format("%s", d12.getText()));
+        }else if(v.getId()==R.id.d10){
+            Button d10 = (Button) v;
+            rollDisplay.append(String.format("%s", d10.getText()));
+        }else if(v.getId()==R.id.d8){
+            Button d8 = (Button) v;
+            rollDisplay.append(String.format("%s", d8.getText()));
+        }else if(v.getId()==R.id.d6){
+            Button d6 = (Button) v;
+            rollDisplay.append(String.format("%s", d6.getText()));
+        }else if(v.getId()==R.id.d4){
+            Button d4 = (Button) v;
+            rollDisplay.append(String.format("%s", d4.getText()));
+        }else if(v.getId()==R.id.btClr){
+            rollDisplay.setText(String.format(""));
+        }
+
+    }
 }
---
+++ readme.md
@@ -0,0 +1,28 @@
+#RollOP
+
+Aplicativo em andamento para o estudo de desenvolvimento mobile Android
+
+###Objeivo
+O objetivo do projeto é desenvolver uma *"calculadora"* para auxiliar na rolagem de dados em um jogo de RPG.
+
+O aplicativo irá contar com:
+ - teclas para os botões padrões
+ - teclado numérico para inserir modificadores
+ - rolagem de multiplos dados
+ - rolagem personalizada
+ - ...
+
+###TODO
+
+- [x] Aplicativo Base
+ - [x] Botões Padões
+ - [x] Teclado Numérico
+ - [x] Barra de Controle
+ - [x] Display de Rolagens
+ - [ ] Melhorias na Base
+- [ ] Funções
+ - [ ] Rolagem Padrão
+ - [ ] Rolagem Personalizada
+ - [ ] Rolagem Multipla
+ - [ ] Melhorias de Funções
+- [ ] Melhorias Gerais
